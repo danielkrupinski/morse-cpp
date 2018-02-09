555c21fb8f8fdaab40678f6962d492e4f0f7aab4
Daniel Krupiński <danielkrupinski@outlook.com>
2018-02-09 19:42:14 +0100

    Add latin digits

--- src/morse.cpp
+++ src/morse.cpp
@@ -6,6 +6,7 @@ string to_morse(const string& s)
 {
 	static const string latin {"abcdefghijklmnopqrstuvwxyz"};
 	static const string latin_capital {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	static const string latin_digits {"0123456789"};
 	static const vector<string> morse {"• −", "− • • •", "− • − •", "− • •", "•", "• • − •", "− − •", "• • • •", "• •", "• − − −", "− • −", "• − • •", "− −", "− •", "− − −", "• − − •", "− − • −", "• − •", "• • •", "−", "• • −", "• • • −", "• − −", "− • • −", "− • − −", "− − • •"};
 	static const vector<string> morse_digits {"− − − − −", "• − − − −", "• • − − −", "• • • − −", "• • • • −", "• • • • •", "− • • • •", "− − • • •", "− − − • •", "− − − − •"};
 }
