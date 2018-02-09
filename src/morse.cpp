37b43b3a680a74607a7d25c3a217cb1fd5267bc2
Daniel Krupiński <danielkrupinski@outlook.com>
2018-02-09 19:40:49 +0100

    Add morse alphabet and digits

--- src/morse.cpp
+++ src/morse.cpp
@@ -6,5 +6,6 @@ string to_morse(const string& s)
 {
 	static const string latin {"abcdefghijklmnopqrstuvwxyz"};
 	static const string latin_capital {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-	static const vector<string> morse {"• —"};
+	static const vector<string> morse {"• −", "− • • •", "− • − •", "− • •", "•", "• • − •", "− − •", "• • • •", "• •", "• − − −", "− • −", "• − • •", "− −", "− •", "− − −", "• − − •", "− − • −", "• − •", "• • •", "−", "• • −", "• • • −", "• − −", "− • • −", "− • − −", "− − • •"};
+	static const vector<string> morse_digits {"− − − − −", "• − − − −", "• • − − −", "• • • − −", "• • • • −", "• • • • •", "− • • • •", "− − • • •", "− − − • •", "− − − − •"};
 }
