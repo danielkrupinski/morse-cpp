4037f515f28c55a91672048c22d6ffe2afa01234
Daniel Krupiński <danielkrupinski@outlook.com>
2018-02-10 16:31:50 +0100

    Remove digits translation

--- src/morse.cpp
+++ src/morse.cpp
@@ -6,7 +6,5 @@ string to_morse(const string& s)
 {
 	static const string latin {"abcdefghijklmnopqrstuvwxyz"};
 	static const string latin_capital {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-	static const string latin_digits {"0123456789"};
 	static const vector<string> morse {"• −", "− • • •", "− • − •", "− • •", "•", "• • − •", "− − •", "• • • •", "• •", "• − − −", "− • −", "• − • •", "− −", "− •", "− − −", "• − − •", "− − • −", "• − •", "• • •", "−", "• • −", "• • • −", "• − −", "− • • −", "− • − −", "− − • •"};
-	static const vector<string> morse_digits {"− − − − −", "• − − − −", "• • − − −", "• • • − −", "• • • • −", "• • • • •", "− • • • •", "− − • • •", "− − − • •", "− − − − •"};
 }
