2ebe8ff91e1ac4a89ee32c1e573e08542e59e6e5
Daniel Krupiński <danielkrupinski@outlook.com>
2018-02-17 16:54:13 +0100

    Update code formatting

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main()
 {
-	cout << "Enter word to convert to morse code: ";
-	string word {};
-	cin >> word;
-	vector<string> in_morse {to_morse(word)};
-	for (auto& a : in_morse)
-		cout << a << ' ';
-	cout << endl;
-	return 0;
+    cout << "Enter word to convert to morse code: ";
+    string word {};
+    cin >> word;
+    vector<string> in_morse {to_morse(word)};
+    for (auto& a : in_morse)
+        cout << a << ' ';
+    cout << endl;
+    return 0;
 }
